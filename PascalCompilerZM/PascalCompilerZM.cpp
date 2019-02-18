dc906827d66afa6c608808a696e92eac13f14cff
misha1020 <32141478+misha1020@users.noreply.github.com>
2019-02-18 09:50:14 +0500

    Считываем ошибки по строкам

--- PascalCompilerZM/PascalCompilerZM.cpp
+++ PascalCompilerZM/PascalCompilerZM.cpp
@@ -5,22 +5,30 @@
 
 using namespace std;
 
+char NextCh();
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	ifstream inFile("input.txt");
-	char symbol;
-	string out = "";
-	inFile >> symbol;
-	while (!inFile.eof())
+	ifstream fErrMsgs("Err.msg");
+	string *errorsArray = new string[500];
+	int errCount = 0;
+	while (getline(fErrMsgs, errorsArray[errCount]))
 	{
-		out += symbol;
-		inFile >> symbol;
+		errCount++;
 	}
-	inFile.close();
+	fErrMsgs.close();
+	//ifstream fPascalCode("PascalCode.txt");
+	//ifstream fErrors("Errors.txt");
 
-	cout << "Считанная строка: " << out << endl; 
+	for (int i = 0; i < errCount; i++)
+		cout << "Считанная строка: " << errorsArray[i] << endl;
 
 	system("pause");
 	return 0;
+}
+
+char NextCh()
+{
+	return 'S';
 }
