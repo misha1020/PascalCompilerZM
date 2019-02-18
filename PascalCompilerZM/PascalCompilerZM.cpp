221e68811c9ca69e918872e48c7182587ceef3a6
misha1020 <32141478+misha1020@users.noreply.github.com>
2019-02-18 21:53:03 +0500

    Модуль ввода-вывода в принципе рабочий
    Подправить вывод ошибок, добавить приветственную строку и по оформлению глянуть...
    Пробелы пофиксить, чтобы читались
    Красивую нумерацию сделать в выходном файле

--- PascalCompilerZM/PascalCompilerZM.cpp
+++ PascalCompilerZM/PascalCompilerZM.cpp
@@ -5,30 +5,82 @@
 
 using namespace std;
 
-char NextCh();
+struct textPosition
+{
+	int lineNumber;
+	int charNumber;
+	int errNumber;
+};
+
+void NextCh(textPosition *errPos, ifstream& inFile, ofstream& outFile, string *errArray);
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	string *errorsArray = new string[500];									// INT_MAX
+	int allErrorsCount = 0;
 	ifstream fErrMsgs("Err.msg");
-	string *errorsArray = new string[500];
-	int errCount = 0;
-	while (getline(fErrMsgs, errorsArray[errCount]))
+	while (getline(fErrMsgs, errorsArray[allErrorsCount]))
 	{
-		errCount++;
+		allErrorsCount++;
 	}
 	fErrMsgs.close();
-	//ifstream fPascalCode("PascalCode.txt");
-	//ifstream fErrors("Errors.txt");
 
-	for (int i = 0; i < errCount; i++)
-		cout << "Считанная строка: " << errorsArray[i] << endl;
+	textPosition errPositions[500];
+	ifstream fErrors("ErrTable.txt");
+	int currErrorsCount = 0;
+	while (!fErrors.eof())
+	{
+		fErrors >> errPositions[currErrorsCount].lineNumber;
+		fErrors >> errPositions[currErrorsCount].charNumber;
+		fErrors >> errPositions[currErrorsCount].errNumber;
+		currErrorsCount++;
+	}
+
+	ofstream fResult;
+	fResult.open("Result.txt");
+	ifstream fPascalCode;
+	fPascalCode.open("PascalCode.txt");
+	NextCh(errPositions, fPascalCode, fResult, errorsArray);
+
+	fResult.close();
+	fPascalCode.close();
+	//for (int i = 0; i < currErrorsCount; i++)
+	//	cout << "Считанная строка: " << errPositions[i].lineNumber << errPositions[i].charNumber << errPositions[i].errNumber << endl;
+	//for (int i = 0; i < allErrorsCount; i++)
+	//	cout << "Считанная строка: " << errorsArray[i] << endl;
 
 	system("pause");
 	return 0;
 }
 
-char NextCh()
+void NextCh(textPosition *errPos, ifstream& inFile, ofstream& outFile, string *errArray)
 {
-	return 'S';
+	int currLineNum = 0;
+	int lastError = 0;
+	string currentLine = "", errorLine = "";
+	char currLiter;
+	while (!inFile.eof())
+	{
+		inFile >> currLiter;
+		currentLine += currLiter;
+		if (currLiter == ' ')
+			cout << "LOL!" << endl;
+		if (inFile.peek() == '\n' || inFile.peek() == EOF)
+		{
+			outFile << currentLine << endl;
+			currentLine = "";
+			while (errPos[lastError].lineNumber - 1 == currLineNum)
+			{
+				for (int i = 0; i < errPos[lastError].charNumber - 1; i++)
+					errorLine += " ";
+				errorLine += "^ ошибка код ";
+				outFile << errorLine << errPos[lastError].errNumber << endl;
+				outFile << errArray[errPos[lastError].errNumber] << endl;
+				lastError++;
+				errorLine = "";
+			}
+			currLineNum++;
+		}
+	}
 }
